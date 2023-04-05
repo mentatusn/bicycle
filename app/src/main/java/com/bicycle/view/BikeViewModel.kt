a65e4a27d9950ceceb9d72988524697fa139923e
Andej Nesterenko <mentatus.n@gmail.com>
2023-04-05 15:19:36 +0300

    refactor: BikeViewModel

--- app/src/main/java/com/bicycle/view/BikeViewModel.kt
+++ app/src/main/java/com/bicycle/view/BikeViewModel.kt
@@ -1,50 +1,55 @@
 package com.bicycle.view
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.bicycle.R
 import com.bicycle.model.Bike
 import com.bicycle.model.StatusBike
 import com.bicycle.repository.BikeRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class BikeViewModel(private val repository: BikeRepository = BikeRepository()) : ViewModel() {
+class BikeViewModel(
+    private val context: Application,
+    private val repository: BikeRepository = BikeRepository()
+) : AndroidViewModel(context) {
     val bikes = repository.bikes
     val appStateLiveData = repository.appStateLiveData
 
     fun pressBike(bike: Bike) {
-        CoroutineScope(Job()).launch {
+        viewModelScope.launch {
             var text = ""
+            val currentTime = System.currentTimeMillis()
             when (bike.status) {
                 StatusBike.IDLE -> {
-                    bike.startTime = System.currentTimeMillis()
+                    bike.startTime = currentTime
                     bike.endTime = bike.rentDuration * 60 * 1000
                     bike.status = StatusBike.ACTIVE
-                    text = "Арендован"
+                    text = context.getString(R.string.bike_rented)
                 }
                 StatusBike.ACTIVE -> {
-                    if ((System.currentTimeMillis() - bike.startTime) / 1000 < 60) {
+                    if ((currentTime - bike.startTime) / 1000 < 60) {
                         bike.endTime = 1
                         bike.status = StatusBike.CANCELED
-                        text = "Отмена"
+                        text = context.getString(R.string.bike_cancelled)
                     } else {
                         bike.endTime = 1
                         bike.status = StatusBike.IDLE
-                        text = "Закрыли"
+                        text = context.getString(R.string.bike_closed)
                     }
                 }
                 StatusBike.WAIT_FOR_CANCEL -> {
                     bike.endTime = 1
                     bike.status = StatusBike.IDLE
-                    text = "Закрыли"
+                    text = context.getString(R.string.bike_closed)
                 }
                 StatusBike.CANCELED -> {
-                    bike.startTime = System.currentTimeMillis()
+                    bike.startTime = currentTime
                     bike.status = StatusBike.ACTIVE
-                    text = "Арендован"
+                    text = context.getString(R.string.bike_rented)
                 }
             }
-            repository.updateBike(bike, text){}
+            repository.updateBike(bike, text) {}
         }
     }
 
