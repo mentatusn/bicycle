fcfc4041e1cb22d341d36f02f2417a93cda240d1
Andej Nesterenko <mentatus.n@gmail.com>
2023-07-23 15:33:17 +0300

    refactor: Warnings

--- app/src/main/java/com/bicycle/util/Const.kt
+++ app/src/main/java/com/bicycle/util/Const.kt
@@ -11,5 +11,6 @@ object Const {
     const val sheet2 = "Общая"
     const val sheet1 = "Активные"
     //const val sheetId = "1JW4gj9kL9yRwOIjE0V8uhNdQNrCH2ioPewwdZkFnf_E"
-    const val sheetId = "1F3CorRDHw8rLaw0ySRKJX6a2h3Qu8h1bmGHAPe7zRXs"
+    //const val sheetId = "1F3CorRDHw8rLaw0ySRKJX6a2h3Qu8h1bmGHAPe7zRXs"
+    const val sheetId = "1CsZ_hEYnFnHGipVhkZydr_I3UPSiyClSbujQsA9J-tY" // это 2
 }
